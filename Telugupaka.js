0fd0080cc9b1a232e544003d338acb726a4965a7
yashwanth2804 <kambaalayashwanth@gmail.com>
2018-04-12 17:44:36 +0530

    Updated logic with grabing LinkShare urls

--- Telugupaka.js
+++ Telugupaka.js
@@ -1,64 +1,129 @@
 const puppeteer = require('puppeteer');
 
 const readline = require('readline');
-
+ var prompt = require('prompt');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-//ask for movie name
-rl.question('Please enter movie name ', (movie_name) => {
-    ///getting Movie name from user
-    let Movie = "+"+movie_name+"+"
-    console.log(`You entered ${Movie}`);
-    ///check if movie exist
-    //var url = "https://duckduckgo.com/?q="+Movie+"site%3Atelugupalaka.com"
-    var url = "https://duckduckgo.com/?q="+Movie+"site%3Atelugupalaka.com";
-    //console.log(url)
-    //check if got any results
-    (async () => {
-      const browser = await puppeteer.launch({headless:false})
-      const page = await browser.newPage()
-      await page.goto(url)
-      const hrefs = await page.$$eval('a.result__a', hrefs => hrefs.map((a) => {
-                return a.href
-                  }));
 
-      var link = hrefs[0];
-     if (typeof link == "undefined") {
-                console.log("Movie with name "+movie_name+" not found");
-                process.exit();
-            }else{
-                //// rest code
-              console.log(link);
-              await page.close();
-              const Telugupalaka_page = await browser.newPage()
-            //  await Telugupalaka_page.setRequestInterceptionEnabled(true);
-            const Telugupalaka_page_response =   await Telugupalaka_page.goto(link,{waitUntil: 'networkidle2'});
-            console.log(Telugupalaka_page_response.headers);
-              //
-              // //await page.setRequestInterceptionEnabled(true);
-              //  Telugupalaka_page.on('request', request => {
-              //         //request.continue();
-              //         console.log("URL: " + request.url);
-              //
-              //     });
-              // Telugupalaka_page.on('request', request => {
-              //        //request.continue();
-              //        console.log("URL: " + request.url);
-              //        console.log("METHOD: " + request.method);
-              //        //console.log("HEADERS:" + JSON.stringify([...request.headers]) + "\n");
-              //    });
+async function DuckDuckGo(url) {
+  try {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+    const page = await browser.newPage();
+
+    //  var url = "https://google.com"
+    await page.goto(url);
+    const hrefs = await page.$$eval('a.result__a', hrefs => hrefs.map((a) => {
+      return a.href
+    }));
+
+    var link = hrefs[0];
+
+    if (typeof link == "undefined") {
+      page.close();
+      await browser.close();
+      return 0;
+    } else {
+      page.close();
+      await browser.close();
+      return link;
+    }
 
 
-            }
+    //  process.exit(1);
+  } catch (err) {
+    console.error(err);
+  }
 
-                  await browser.close()
-      })()
+}
+
+///Telugu palaka Page
+async function TelugupalakaPage(url) {
+
+
+  try {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+    const page = await browser.newPage();
+
+	await page.setRequestInterception(true);
+	page.on('request', (request) => {
+		if (['image', 'stylesheet', 'font', 'script'].indexOf(request.resourceType()) !== -1) {
+			request.abort();
+		} else {
+			request.continue();
+		}
+	});
+
+
+    await page.goto(url);
+	const hrefs = await page.$$eval('a[target]', aTags => aTags.map(a =>
+	{
+		let textC = a.textContent;
+
+			if(textC.match(/.*Watch.*/)){
+				var rep = textC.replace('Watch', 'Download');
+				 return rep+"->"+a.getAttribute("href");
+			}
+	}
+	));
+
+	var f = hrefs.filter(item => item);
+	 let y=0;
+	 for(let g of f)
+		console.log("  "+(y++) + " "+ g);
+
+  //  var  selectedUrl = f[1].toString().split("->")[1];
+    // return selectedUrl;
+    prompt.start();
+    prompt.get(['SelectOption'], function (err, result) {
+
+       var  selectedUrl = f[result.SelectOption].toString().split("->")[1];
+       console.log(selectedUrl);
+      });
+
+
+	 // rl.question('@Select one from list', (answer) => {
+   //      console.log("Seletec index"+answer);
+		// 		var  selectedUrl = f[answer].toString().split("->")[1];
+		// 		console.log("Selected Url"+selectedUrl);
+   //      return selectedUrl;
+   //
+	 // });
+
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+
+///Linkshare.net page
 
 
 
 
-    rl.close();
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  answer = "+" + answer + "+";
+  var url = "https://duckduckgo.com/?q=" + answer + "site%3Atelugupalaka.com";
+
+  DuckDuckGo(url).then(a => {
+
+    if (a == 0) {
+      console.log("No Url found");
+      process.exit(1);
+    } else {
+      console.log("url found " + a);
+      TelugupalakaPage(a);
+
+    }
+
+  });
+
+  rl.close();
 });
