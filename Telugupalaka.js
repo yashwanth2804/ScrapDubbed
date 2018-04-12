58238eaa285acef3377e36f38e15f9d5bb125d6a
yashwanth2804 <kambaalayashwanth@gmail.com>
2018-04-12 22:40:33 +0530

    Updated with Regex call

--- Telugupalaka.js
+++ Telugupalaka.js
@@ -16,7 +16,7 @@ async function DuckDuckGo(url) {
     const page = await browser.newPage();
 
     //  var url = "https://google.com"
-    await page.goto(url);
+    await page.goto(url,{timeout: 0});
     const hrefs = await page.$$eval('a.result__a', hrefs => hrefs.map((a) => {
       return a.href
     }));
@@ -57,11 +57,11 @@ async function TelugupalakaPage(url) {
 			request.abort();
 		} else {
 			request.continue();
-		} 
+		}
 	});
 
 
-    await page.goto(url);
+    await page.goto(url,{timeout: 0});
 	const hrefs = await page.$$eval('a[target]', aTags => aTags.map(a =>
 	{
 		let textC = a.textContent;
@@ -78,13 +78,18 @@ async function TelugupalakaPage(url) {
 	 for(let g of f)
 		console.log("  "+(y++) + " "+ g);
 
+
+    //page.close();
+    //await browser.close();
   //  var  selectedUrl = f[1].toString().split("->")[1];
     // return selectedUrl;
     prompt.start();
     prompt.get(['SelectOption'], function (err, result) {
 
        var  selectedUrl = f[result.SelectOption].toString().split("->")[1];
-       console.log(selectedUrl);
+       //console.log(selectedUrl);
+
+       LinkSharenet(selectedUrl);
       });
 
 
@@ -103,7 +108,42 @@ async function TelugupalakaPage(url) {
 }
 
 ///Linkshare.net page
+async function LinkSharenet(url) {
 
+
+  try {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+    const page = await browser.newPage();
+
+	await page.setRequestInterception(true);
+	page.on('request', (request) => {
+		if (['image', 'stylesheet', 'font', 'script'].indexOf(request.resourceType()) !== -1) {
+			request.abort();
+		} else {
+			request.continue();
+		}
+	});
+
+
+    await page.goto(url,{timeout: 0});
+    //get the recV from scrapping
+      let bodyHTML = await page.evaluate(() => document.body.textContent);
+
+
+      var reg =/(revC\(".*"\))/;
+      var m1 = bodyHTML.toString().match(reg);
+      var m11=m1[0];
+      var m2 = m11.toString().match(/(".*")/);
+      console.log(m11+"  "+m2[0]);
+
+
+  } catch (err) {
+    console.error(err);
+  }
+
+}
 
 
 
